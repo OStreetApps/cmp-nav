addf687e499f1691984ec70c41f8f0a0cc4f3d96
DavinRot <68320463+DavinRInterview@users.noreply.github.com>
2023-10-03 18:55:35 -0700

    update build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,24 @@ plugins {
     id("com.android.application").version("8.1.1").apply(false)
     id("com.android.library") version "8.1.1" apply false
     id("org.jetbrains.compose").apply(false)
+    id("maven-publish")
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+    }
+
+
+}
+
+allprojects {
+
+    group = "io.github.ostreetapps"
+    version = "1.0"
+
 }
 
 tasks.register("clean", Delete::class) {
